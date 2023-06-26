3895cf0417bca03b954112258ec1836472a3f3a0
s-c-meyer <meyer.samuelc@gmail.com>
2023-06-26 15:24:01 -0700

    Added basic structure for the app
    Added a documentation.html page along with a topMovies JSON object that shows the top movies. Also removed the index.html page from previous exercises.

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express'), 
+  morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+//JSON Object containing top 10 movies
+let topMovies = [
+  {
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'Arrival',
+    director: 'Denis Villeneuve'
+  },
+  {
+    title: 'Moonlight',
+    director: 'Barry Jenkins'
+  },
+  {
+    title: 'Manchester by the Sea',
+    director: 'Kenneth Lonergan'
+  },
+  {
+    title: 'Star Wars Episode III - Revenge of the Sith',
+    director: 'George Lucas'
+  },
+  {
+    title: 'Harry Potter and the Goblet of Fire',
+    director: 'Mike Newell'
+  },
+  {
+    title: 'Anchorman: The Legend of Ron Burgundy',
+    director: 'Adam McKay'
+  },
+  {
+    title: 'Elf',
+    director: 'Jon Favreau'
+  },
+  {
+    title: 'Men In Black',
+    director: 'Barry Sonnenfeld'
+  }
+];
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie app, MyFlix!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.use(express.static('public'));
+
+//error handling middleware should always be defined last in a chain of middleware, but before the app.listen()
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Somrthing has gone wrong!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080');
+});
