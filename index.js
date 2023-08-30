e07937fd1fd5cb3807d50e14eec02c25a80be46a
s-c-meyer <meyer.samuelc@gmail.com>
2023-08-30 11:36:21 -0500

    Re-added Authentication for /movies

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ passport.authenticate('jwt', { session: false }), (req, res) => {
 });
 
 //GET all Movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
