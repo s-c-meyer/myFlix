3be5fc869b3111ec249ddf8afa5a9fbe96138a2e
s-c-meyer <meyer.samuelc@gmail.com>
2023-10-12 12:57:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -95,12 +95,12 @@ app.post('/users',
 //UPDATE a user's info, by username
 //this does not require all of the same validation as before, because 
 app.put('/users/:Username', 
-  [
-    check('Username', 'Username must be 5 or more characters').isLength({min: 5}),
-    check('Username', 'Username must contain only alpha-numeric characters').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail()
-  ],
+  // [
+  //   check('Username', 'Username must be 5 or more characters').isLength({min: 5}),
+  //   check('Username', 'Username must contain only alpha-numeric characters').isAlphanumeric(),
+  //   check('Password', 'Password is required').not().isEmpty(),
+  //   check('Email', 'Email does not appear to be valid').isEmail()
+  // ],
 passport.authenticate('jwt', { session: false }), (req, res) => {
   //Ensure that one user cannot edit another user's data
   if(req.user.Username !== req.params.Username){ 
